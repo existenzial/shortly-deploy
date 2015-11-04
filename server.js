abd178c507045e3dfc0fb35d8ea01ac6c5a1c7bf
TGAStudent <tga.student@telegraphacademy.com>
2015-11-04 15:38:42 -0800

    add ignore file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+var port = process.env.PORT || 4568;
 
 app.listen(port);
 
-console.log('Server now listening on port ' + port);
+console.log('Server now listening on port '+ port);
