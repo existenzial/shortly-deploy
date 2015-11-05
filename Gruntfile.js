aeee8e6313cfc3215d047270a53b210fadabe830
TGAStudent <tga.student@telegraphacademy.com>
2015-11-04 17:23:59 -0800

    staged everything, forgot where we made edits in tiny files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,23 @@
 module.exports = function(grunt) {
 
+  // All configuration goes here
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      dist: {
+          src: [
+              'server.js',
+              'server-config.js',
+              'app/**/*.js', // All JS in the libs folder
+              'public/**/*.js',
+              'lib/*.js',
+              'views/*.ejs'
+
+              // 'someDir/global.js',  // This specific file
+
+          ],
+          dest: 'js/build/production.js',
+      }
     },
 
     mochaTest: {
@@ -21,24 +36,45 @@ module.exports = function(grunt) {
     },
 
     uglify: {
+      build: {
+          src: 'js/build/production.js',
+          dest: 'js/build/production.min.js'
+      }
+    //not sure if necessary (original code)
+      // files: [
+        
+
+
+        
+      // ],
+      // options: {
+
+      // }
     },
 
     jshint: {
       files: [
         // Add filespec list here
+        '/**/*.js'
       ],
       options: {
         force: 'true',
         jshintrc: '.jshintrc',
         ignores: [
           'public/lib/**/*.js',
-          'public/dist/**/*.js'
+          'public/dist/**/*.js',
+          'node_modules/**/*.js',
+          'test/**/*.js'
         ]
       }
     },
 
     cssmin: {
         // Add filespec list here
+      css:{
+        src: 'public/style.css',
+        dest: 'views/concat.min.css'
+       }
     },
 
     watch: {
@@ -64,6 +100,7 @@ module.exports = function(grunt) {
     },
   });
 
+  //Where we tell Grunt we plan to use this plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -73,6 +110,10 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-shell');
   grunt.loadNpmTasks('grunt-nodemon');
 
+
+
+
+  // Where we tell Grunt what to do when we type 'grunt' into the terminal
   grunt.registerTask('server-dev', function (target) {
     // Running nodejs in a different process and displaying output on the main console
     var nodemon = grunt.util.spawn({
@@ -108,6 +149,8 @@ module.exports = function(grunt) {
   grunt.registerTask('deploy', [
       // add your production server task here
   ]);
-
+//suppose to watch out for jshint fails and abort grunt build process. 
+//most likely ned to build an if condition to check if jsHint fails then run the bottom code. 
+  grunt.fail.fatal("Most likely jshint failed to load up"); 
 
 };
